bb1ac54d629f233a2801730ef146cc12dca4a224
Zachary Proser <zackproser@gmail.com>
2023-10-06 14:26:30 +0000

    Create ECR registry. Add comments

--- index.ts
+++ index.ts
@@ -4,12 +4,35 @@ import * as awsx from "@pulumi/awsx";
 
 // Context: see https://www.notion.so/AWS-Pinecone-Reference-Architecture-in-Pulumi-PRD-61245ccff1f040499b5e2417f92eee77
 
+
+/**
+ * Supporting resources
+ */
 // Create an S3 bucket to store video frames
 const bucket = new aws.s3.Bucket("input-bucket");
 
-const firstWorkerServer = new aws.ec2.Instance("worker1", { ami: "ami-067d1e60475437da2", instanceType: "t2.micro" })
+// Create an Elastic Container Registry (ECR) to hold Docker images we plan to ship for the workers
+const registry = new aws.ecr.Repository("docker-registry")
 
-// Export the name of the bucket
+/**
+ * Worker tier
+ */
+// Create an array to hold the worker servers we've created
+let workerServers = [];
+
+// Create 4 worker EC2 instances, all using the same Amazon Machine Image (AMI) and instance type
+for (var i = 0; i < 4; i++) {
+  const workerServer = new aws.ec2.Instance(`worker-${i}`, { ami: "ami-067d1e60475437da2", instanceType: "t2.micro" })
+  workerServers.push(workerServer.id)
+}
+
+/**
+ * Exports
+ *
+ * Whatever values are exported here will be output in pulumi's terminal output that displays following an update:
+ */
 export const bucketName = bucket.id;
-export const workerInstanceId = firstWorkerServer.id
+export const registryId = registry.id
+export const workerInstanceIds = workerServers.forEach((id) => id)
+
 
