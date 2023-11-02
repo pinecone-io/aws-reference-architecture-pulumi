776ec65f8ab9ed3eadd56e8c980fb7179a0130ec
Zachary Proser <zackproser@gmail.com>
2023-11-02 21:05:34 +0000

    Don't require certificate via client. Update default db name -Create new security group for pelican

--- pelican/src/index.ts
+++ pelican/src/index.ts
@@ -1,15 +1,13 @@
-import { Client } from "pg";
+import { Client, Notification } from "pg";
 import process from "process";
 import { SQS } from "aws-sdk";
 import checkEnvVars from "./utils";
 
-type ChangeMessage = {
-  channel: string;
-  payload: string;
-};
-
 let client: Client;
 const sqs = new SQS({ region: process.env.AWS_REGION });
+
+// Ensure all required environment variables are set before starting up
+checkEnvVars();
 
 async function connectToDatabase() {
   client = new Client({
@@ -17,7 +15,10 @@ async function connectToDatabase() {
     host: process.env.POSTGRES_DB_HOST,
     database: process.env.POSTGRES_DB_NAME,
     password: process.env.POSTGRES_DB_PASSWORD,
-    port: process.env.POSTGRES_DB_PORT as unknown as number,
+    port: Number(process.env.POSTGRES_DB_PORT),
+    ssl: {
+      rejectUnauthorized: false,
+    }
   });
 
   client.on("error", (err: Error) => {
@@ -37,26 +38,51 @@ async function connectToDatabase() {
       "Pelican: Database connected successfully. Listening for changes...",
     );
 
-    // eslint-disable-next-line
-    (client as any).on("notification", async (message: ChangeMessage) => {
-      if (message.channel === "table_change") {
-        const payload = JSON.parse(message.payload);
-        console.log("Change detected:", payload);
-
-        await forwardMessageToQueue(message);
-      }
-    });
+    /* eslint-disable  @typescript-eslint/no-explicit-any */
+    (client as any).on("notification", handleNotification);
+    await listenForTableChanges();
   } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.error("Failed to connect to database:", err.stack);
+    handleDatabaseConnectionError(err);
+    reconnectToDatabase();
+  }
+}
+
+async function listenForTableChanges() {
+  try {
+    await client.query("LISTEN table_change");
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error listening to table changes:", error.stack);
     } else {
-      console.error("An unexpected error occurred:", err);
+      console.error("An unexpected error occurred:", error);
     }
     reconnectToDatabase();
   }
+}
 
-  // Listen for notifications of changes
-  client.query("LISTEN table_change");
+function handleDatabaseConnectionError(err: unknown) {
+  if (err instanceof Error) {
+    console.error("Failed to connect to database:", err.stack);
+  } else {
+    console.error("An unexpected error occurred:", err);
+  }
+}
+
+function handleNotification(message: Notification) {
+  try {
+    if (message.channel === "table_change") {
+      const payload = JSON.parse(message.payload as string);
+      console.log("Change detected:", payload);
+
+      forwardMessageToQueue(message);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error handling notification:", error.stack);
+    } else {
+      console.error("An unexpected error occurred:", error);
+    }
+  }
 }
 
 function reconnectToDatabase() {
@@ -64,12 +90,9 @@ function reconnectToDatabase() {
   setTimeout(connectToDatabase, 5000); // Retry connection every 5 seconds
 }
 
-// Ensure all required environment variables are set before starting up
-checkEnvVars();
-
 connectToDatabase();
 
-async function forwardMessageToQueue(message: ChangeMessage) {
+async function forwardMessageToQueue(message: Notification) {
   try {
     const params = {
       QueueUrl: process.env.SQS_QUEUE_URL!,
@@ -77,8 +100,12 @@ async function forwardMessageToQueue(message: ChangeMessage) {
     };
     const result = await sqs.sendMessage(params).promise();
     console.log(`Message sent to SQS queue, message ID: ${result.MessageId}`);
-  } catch (error) {
-    console.error("Error sending", error);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error sending message to SQS:", error.stack);
+    } else {
+      console.error("An unexpected error occurred:", error);
+    }
   }
 }
 
@@ -87,4 +114,9 @@ process.on("SIGINT", async () => {
   await client.end();
   console.log("Database connection closed on app termination");
   process.exit();
+});
+
+// Global Unhandled Rejection Listener
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
 });
