a6ae32e350602db6c1ee7262d87af82434531c8b
Zachary Proser <zackproser@gmail.com>
2023-11-07 16:02:39 +0000

    Allow Emu to read SQS messages from job queue

--- emu/src/index.ts
+++ emu/src/index.ts
@@ -1,16 +1,18 @@
 import AWS, { AWSError, SQS } from "aws-sdk";
 import { config } from "dotenv";
 
+import {
+  RecordMetadata,
+  RecordMetadataValue,
+} from "@pinecone-database/pinecone";
+
 import { EmbedderInput } from "./embedder";
 import { orchestrate } from "./orchestrator";
-import { ReceiveMessageResult } from "aws-sdk/clients/sqs";
+import { ReceiveMessageResult, Message } from "aws-sdk/clients/sqs";
 config();
 
 const checkEnvVars = () => {
-  const requiredVars = [
-    "AWS_REGION",
-    "SQS_QUEUE_URL",
-  ];
+  const requiredVars = ["AWS_REGION", "SQS_QUEUE_URL"];
   requiredVars.forEach((varName) => {
     if (!process.env[varName]) {
       throw new Error(`Environment variable ${varName} is not defined`);
@@ -38,29 +40,55 @@ const params = {
   WaitTimeSeconds: 0,
 };
 
-const handleMessages = async function(
-  err: AWSError,
-  data: ReceiveMessageResult,
+const handleMessages = async function (
+  err: AWSError | null, // Error should be AWSError or null
+  data: ReceiveMessageResult, // Data should be of type ReceiveMessageResult
 ) {
+  console.log("handleMessages: ");
   if (err) {
     console.log("Receive Error", err);
   } else if (data.Messages) {
-    const inputs: EmbedderInput[] = [];
-    data.Messages.forEach((message) => {
-      const payload = JSON.parse(message.Body!).payload;
+    const inputs: EmbedderInput[] = []; // Explicitly type the array as EmbedderInput[]
+    data.Messages.forEach((message: Message) => {
+      // Message should be of type Message
+      // First, parse the Body to get the payload string
+      const body = JSON.parse(message.Body as string); // Assert that Body is a string
+      // Then, parse the payload string to get the actual object
+      const payload = JSON.parse(body.payload);
+      console.log("payload: %o", payload);
 
       inputs.push({
-        id: payload.new.id,
+        id: payload.new.id.toString(),
         text: payload.new.description,
-        metadata: { ...payload.new },
+        metadata: Object.entries(payload.new).reduce((meta, [key, value]) => {
+          // Skip null and undefined values
+          if (value !== null && value !== undefined) {
+            // Assert that value is of type RecordMetadataValue
+            const metadataValue: RecordMetadataValue = Array.isArray(value)
+              ? value.filter((item): item is string => typeof item === "string") // Keep only strings in arrays
+              : typeof value === "string" ||
+                typeof value === "number" ||
+                typeof value === "boolean"
+              ? value
+              : value.toString(); // Convert other types to string
+            // TypeScript needs assertion for key to be of type keyof T where T extends RecordMetadata
+            meta[key as keyof RecordMetadata] = metadataValue;
+          }
+          return meta;
+        }, {} as RecordMetadata), // Assert the accumulator as RecordMetadata
       });
 
-      //TODO: Delete messages
+      // TODO: Delete messages
+      // Remember to handle message deletion after processing
     });
 
     const mode = "serial";
 
-    await orchestrate<Metadata>(inputs, mode);
+    console.log(`inputs: %o`, inputs);
+
+    // Assuming orchestrate function is correctly defined and imported
+    // Also assuming that the EmbedderInput and Metadata types are correctly defined
+    await orchestrate(inputs, mode);
   }
 };
 
