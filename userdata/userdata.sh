1e5eaea78232223ed053b57aa2780bbff0e06ee1
Zachary Proser <zackproser@gmail.com>
2023-11-15 14:59:40 +0000

    Fix Pinecone client crash for Emu at runtime -Remove unnecessary files and env var

--- userdata/userdata.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/sh
-
-# This user data script is executed on the AWS EC2 instance at the end of its boot sequence
-# user data scripts are appropriate places to issue arbitrary setup or scaffolding commands 
-# such as joining a cluster, pulling and running a docker image, etc
-
-# TODO - implement our desired logic in this script
-
-echo "Hello world"
